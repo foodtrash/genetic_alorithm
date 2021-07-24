e1f6fd2b9595d272d33b59552ec8c04f63998216
foodtrash <faceroll127@gmail.com>
2021-07-25 01:43:41 +0600

    app to run

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
-from classic_alg import Classic
+from classic import Classic
+from genetic import Genetic
 
 if __name__ == "__main__":
   a = Classic(2, -5, 47, -3, 4, 0)
   a.run()  
+  b = Genetic(2, -5, 47, -3, 4, a.f_max, a.f_min, 0) 
+  b.run()
